837917c217aa965f930759b20ba12d5216525e22
Md. Minhazur Rahman <107420699+robot-76@users.noreply.github.com>
2024-04-16 19:31:44 +0900

    updated date 16 April 2014

---
+++ coco_dataset_detr_model/matcher.py
@@ -0,0 +1,129 @@
+# ------------------------------------------------------------------------
+# DAB-DETR
+# Copyright (c) 2022 IDEA. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from Conditional DETR (https://github.com/Atten4Vis/ConditionalDETR)
+# Copyright (c) 2021 Microsoft. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# ------------------------------------------------------------------------
+
+import torch
+from scipy.optimize import linear_sum_assignment
+from torch import nn
+
+from util.box_ops import box_cxcywh_to_xyxy, generalized_box_iou, box_xyxy_to_cxcywh
+
+
+class HungarianMatcher(nn.Module):
+    """This class computes an assignment between the targets and the predictions of the network
+    For efficiency reasons, the targets don't include the no_object. Because of this, in general,
+    there are more predictions than targets. In this case, we do a 1-to-1 matching of the best predictions,
+    while the others are un-matched (and thus treated as non-objects).
+    """
+
+    def __init__(self, cost_class: float = 1, cost_bbox: float = 1, cost_giou: float = 1, focal_alpha = 0.25):
+        """Creates the matcher
+        Params:
+            cost_class: This is the relative weight of the classification error in the matching cost
+            cost_bbox: This is the relative weight of the L1 error of the bounding box coordinates in the matching cost
+            cost_giou: This is the relative weight of the giou loss of the bounding box in the matching cost
+        """
+        super().__init__()
+        self.cost_class = cost_class
+        self.cost_bbox = cost_bbox
+        self.cost_giou = cost_giou
+        assert cost_class != 0 or cost_bbox != 0 or cost_giou != 0, "all costs cant be 0"
+
+        self.focal_alpha = focal_alpha
+
+    @torch.no_grad()
+    def forward(self, outputs, targets):
+        """ Performs the matching
+        Params:
+            outputs: This is a dict that contains at least these entries:
+                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
+                 "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
+            targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
+                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
+                           objects in the target) containing the class labels
+                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
+        Returns:
+            A list of size batch_size, containing tuples of (index_i, index_j) where:
+                - index_i is the indices of the selected predictions (in order)
+                - index_j is the indices of the corresponding selected targets (in order)
+            For each batch element, it holds:
+                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
+        """
+        bs, num_queries = outputs["pred_logits"].shape[:2]
+
+        # We flatten to compute the cost matrices in a batch
+        out_prob = outputs["pred_logits"].flatten(0, 1).sigmoid()  # [batch_size * num_queries, num_classes]
+        out_bbox = outputs["pred_boxes"] # [batch_size * num_queries, 4]
+        #print ("output boxes", out_bbox)
+        out_bbox = out_bbox.flatten(0, 1)
+        #print ("output boxes", out_bbox.shape)
+
+        # Also concat the target labels and boxes
+        tgt_ids = torch.cat([v["labels"] for v in targets])
+        tgt_bbox = torch.cat([v["boxes"] for v in targets])
+        #print("targets boxes", tgt_bbox)
+        #print ("target boxes", tgt_bbox)
+
+        # Compute the classification cost.
+        alpha = self.focal_alpha
+        gamma = 2.0
+        neg_cost_class = (1 - alpha) * (out_prob ** gamma) * (-(1 - out_prob + 1e-8).log())
+        pos_cost_class = alpha * ((1 - out_prob) ** gamma) * (-(out_prob + 1e-8).log())
+        cost_class = pos_cost_class[:, tgt_ids] - neg_cost_class[:, tgt_ids]
+
+        # Compute the L1 cost between boxes
+        cost_bbox = torch.cdist(out_bbox, tgt_bbox, p=1)
+        #print ("output out bboxes", out_bbox.shape)
+        # Compute the giou cost betwen boxes
+        # import ipdb; ipdb.set_trace()
+        cost_giou = -generalized_box_iou(box_cxcywh_to_xyxy(out_bbox), box_cxcywh_to_xyxy(tgt_bbox))
+        #print ("output cost giou", cost_giou)
+        #print ("output cost bboxes", cost_bbox.shape)
+        #print ("output cost_class", cost_class.shape)
+        
+        # Final cost matrix
+        cost_g = self.cost_giou * cost_giou
+        
+        if cost_g.shape != cost_class.shape:
+            # Calculate the mean value of cost_giou
+            mean_giou = 0
+
+            # Calculate the difference in the first dimension
+            diff = cost_class.shape[0] - cost_g.shape[0]
+
+            # Create a new tensor filled with the 0
+            filler = torch.full((diff, cost_g.shape[1]), mean_giou, device=cost_g.device)
+
+            # Concatenate cost_giou with the new tensor
+            cost_gg = torch.cat([cost_g, filler])
+            #print ("output cost g", cost_gg)
+        else:
+            cost_gg = cost_g
+        
+        C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + cost_gg
+    
+        C = C.view(bs, num_queries, -1).cpu()
+        C[C.isnan()] = 0
+        #print ("output C", C)
+
+        sizes = [len(v["boxes"]) for v in targets]
+        indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
+        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
+
+
+def build_matcher(set_cost_class, set_cost_bbox, set_cost_giou, focal_alpha):
+    return HungarianMatcher(
+        cost_class=set_cost_class, 
+        cost_bbox=set_cost_bbox, 
+        cost_giou=set_cost_giou,
+        focal_alpha=focal_alpha
+    )
